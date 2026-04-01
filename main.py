ecf992c304dd674101bcf6c58214822f22ac2971
Artem O <a.oganesyants@gmail.com>
2026-04-01 20:06:53 +0000

    First GET and POST

--- main.py
+++ main.py
@@ -1,12 +1,55 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
 import uvicorn
 
 app = FastAPI()
 
+BOOKS = {
+    "Pomadoro": ["Frank", "1990"],
+    "Dune": ["Herbert", "1950"]
+}
+
+
 @app.get("/health")
 def health_check():
     return Response(status_code=200)
+
+@app.get("/books")
+def get_books():
+    return BOOKS
+
+
+@app.get("/books/{name}")
+def get_book(book_name: str | None = None):
+    if book_name is None:
+          raise HTTPException(
+            status_code = 404,
+            detail=f"Book_name is empty"
+        )
+    if book_name not in BOOKS:
+        raise HTTPException(
+            status_code = 404,
+            detail=f"Book '{book_name}' not found"
+        )
+    
+    return BOOKS[book_name]
+
+@app.post("/book/{name}")
+def add_book(name: str, author: str, year: str):
+    
+    if name in BOOKS:
+        raise HTTPException(
+            status_code= 404,
+            detail=f"Book {name} already exiest"
+        )
+    
+    BOOKS[name] = [author, year]
+
+    return {
+        "name": name,
+        "author": author,
+        "year": year
+    }
 
 
 def run() -> None:
