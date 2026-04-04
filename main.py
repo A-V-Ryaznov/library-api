00baa67e5f811c7fd6e751e06a7983a5aa0473e7
Artem O <a.oganesyants@gmail.com>
2026-04-04 18:45:03 +0000

    add validate, change main file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 from datetime import date
+from decimal import Decimal
 import uvicorn
 
 app = FastAPI()
@@ -9,66 +10,83 @@ app = FastAPI()
 
 """
 BOOKS
-
 name
 author
 year
-create_at 
-warranty
-borrowed_at
-
+cost
 """
 
 BOOKS = {
-    "Pomadoro": ["Frank", "1990"],
-    "Dune": ["Herbert", "1950"]
+    "Pomadoro": ["Frank", 1990, 500.82],
+    "Dune": ["Herbert", 1950, 300.64]
 }
 
-class OperationRequests(BaseModel):
-    book_name: str
-    author: str
-    year: str
+class BookRequests(BaseModel):
+    name: str = Field(..., max_length=255)
+    author: str = Field(..., max_length=255)
+    year: int
+    cost: Decimal = Field(max_digits=10, decimal_places=2)
 
 
+    @field_validator("name")
+    def book_name_not_empty(cls, v:str) -> str:
+        v = v.strip()
+
+        if not v:
+            raise ValueError("Book name cannot be empty")
+        
+        return v
+
+
+    @field_validator("cost")
+    def cost_must_be_positive(cls, v: Decimal) -> Decimal:
+        if v <= 0.0:
+            raise ValueError("Cost must be positive")
+        return v
+        
+    
+
+
+# Get All books
 @app.get("/health")
 def health_check():
     return Response(status_code=200)
 
+# Get All Books
 @app.get("/books")
 def get_books():
     return BOOKS
 
-
 @app.get("/books/{name}")
-def get_book(book_name: str | None = None):
-    if book_name is None:
+def get_book(book: str | None = None):
+    if book is None:
           raise HTTPException(
             status_code = 404,
             detail=f"Book_name is empty"
         )
-    if book_name not in BOOKS:
+    if book not in BOOKS:
         raise HTTPException(
             status_code = 404,
-            detail=f"Book '{book_name}' not found"
+            detail=f"Book '{book}' not found"
         )
     
-    return BOOKS[book_name]
+    return BOOKS[book]
 
-@app.post("/book/{name}")
-def create_book(name: str, author: str, year: str):
-    if name in BOOKS:
+@app.post("/book")
+def create_book(book: BookRequests):
+    if book.name in BOOKS:
         raise HTTPException(
             status_code=409,
-            detail=f"Book '{name}' already exists"
+            detail=f"Book '{book.name}' already exists"
         )
 
-    BOOKS[name] = [author, year]
-    #BOOKS[name] = initial_book
+    BOOKS[book.name] = [book.author, book.year, book.cost]
 
-    return {
-        "message": f"Book '{name}' was created",
-        "book": name,
-        "author": author,
-        "year": year
+    return { 
+        "message": f"Book '{book.name}' was created",
+        "book": book.name,
+        "author": book.author,
+        "year": book.year,
+        "cost": book.cost
     }
 
