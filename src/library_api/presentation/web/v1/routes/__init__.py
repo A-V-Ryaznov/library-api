01c434076dd050aa0b4a93a0cd7e1323ae5f5a7d
Artem O <a.oganesyants@gmail.com>
2026-04-21 20:29:38 +0000

    Add ugly basic auth endpoint

--- src/library_api/presentation/web/v1/routes/__init__.py
+++ src/library_api/presentation/web/v1/routes/__init__.py
@@ -3,11 +3,13 @@ from fastapi import APIRouter
 from .book import router as book_router
 from .health import router as health_router
 from .user import router as user_router
+from .auth import router as auth_router
 
 main_router = APIRouter()
 main_router.include_router(health_router)
 main_router.include_router(book_router)
 main_router.include_router(user_router)
+main_router.include_router(auth_router)
 
 
 __all__ = ["main_router"]
---
+++ src/library_api/presentation/web/v1/routes/auth.py
@@ -0,0 +1,47 @@
+import secrets
+
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from typing import Annotated
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+router = APIRouter()
+
+security = HTTPBasic()
+
+
+basic_auth_data = {
+    "test": "test"
+}
+
+def check_auth_user(
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+):
+    
+    unauthed_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        headers={"WWW-Authenticate": "Basic"}
+    )
+
+    correct_password = basic_auth_data.get(credentials.username)
+
+    if correct_password is None:
+        raise unauthed_exc
+    
+    if not secrets.compare_digest(
+        credentials.password.encode("utf-8"),
+        correct_password.encode("utf-8")
+    ):
+        raise unauthed_exc
+
+    return credentials.username
+
+
+@router.get("/basic-auth")
+def basic_auth( 
+    username: str = Depends(check_auth_user)
+):
+    return{
+        "message": f"Hi {username}",
+        "username": username
+    }
